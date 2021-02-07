77cc67311b6829b72076fe694d1764b0823631b6
Srajan Jaiswal <68801705+Srajan-Jaiswal@users.noreply.github.com>
2021-02-07 16:26:25 +0530

    Delete koderr directory

--- koderr/android/app/src/main/kotlin/com/example/koderr/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.koderr
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- koderr/lib/utils/keys.dart
+++
@@ -1,1 +0,0 @@
-const String API_KEY = 'AIzaSyA4GG4FVMBOYQFGjGRCmU9ApifeT72lF8U';
